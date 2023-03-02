a51f9493ac51787572c99e7234ce92bc0e1a1668
EZillman <evelina.zillman@medieinstitutet.se>
2023-03-02 16:20:33 +0100

    printing user

---
+++ script.js
@@ -0,0 +1,41 @@
+let userList = document.getElementById('userList');
+let newUsername = document.getElementById('newUsername');
+let saveUserBtn = document.getElementById('saveUserBtn');
+
+
+fetch('http://localhost:3000/users/' + getUser)
+.then(res => res.json())
+.then(data => {
+    printUsers(data);
+});
+
+function printUsers(users) {
+    console.log(users);
+
+    userList.innerHTML = '';
+
+    users.map(user => {
+        let li = document.createElement('li');
+        li.id = user.id;
+        li.innerText = user.name;
+        userList.appendChild(li);
+    });
+};
+
+saveUserBtn.addEventListener('click', () => {
+    let user = {name: newUsername.value};
+    console.log(user);
+
+    fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user)
+    })
+    .then(res => res.json())
+    .then(data => {
+        printUsers(data);
+    });
+});
+    
