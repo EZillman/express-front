84b8ff2763df74048778fb4af4af215d4904ca15
EZillman <evelina.zillman@medieinstitutet.se>
2023-03-03 14:08:20 +0100

    added input

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let userList = document.getElementById('userList');
 let newUsername = document.getElementById('newUsername');
+let newPassword = document.getElementById('newPassword');
 let saveUserBtn = document.getElementById('saveUserBtn');
 
 
-fetch('http://localhost:3000/users/' + getUser)
+fetch('http://localhost:3000/users')
 .then(res => res.json())
 .then(data => {
     printUsers(data);
@@ -23,7 +24,7 @@ function printUsers(users) {
 };
 
 saveUserBtn.addEventListener('click', () => {
-    let user = {name: newUsername.value};
+    let user = {name: newUsername.value, password: newPassword.value};
     console.log(user);
 
     fetch('http://localhost:3000/users', {
